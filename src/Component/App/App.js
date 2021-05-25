01cbecd6c4a4f5b822a693ed7cbbfcbeed7f5427
KhaledAlabssi <khaled.absi5@gmail.com>
2021-05-25 21:44:19 +0200

    creating tracklist and track commponent and add style

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import {SearchBar} from '../SearchBar/SearchBar'
 import {SearchResults} from '../SearchResults/SearchResults'
+import {Playlist} from '../Playlist/Playlist'
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
     <div className="App-playlist">
       <SearchResults />
       
-      {/* <!-- Add a Playlist component --> */}
+      <Playlist />
     </div>
   </div>
 </div>
