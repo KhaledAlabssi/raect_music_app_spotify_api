e3cabb808e3987fa8e5aa25d7bd06a6722ae48fa
KhaledAlabssi <khaled.absi5@gmail.com>
2021-05-25 21:26:47 +0200

    creating app, searchbar, searchresults components

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './App.css';
+import {SearchBar} from '../SearchBar/SearchBar'
+import {SearchResults} from '../SearchResults/SearchResults'
 
 function App() {
   return (
     <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-    {/* <!-- Add a SearchBar component --> */}
+  <SearchBar />
     <div className="App-playlist">
-      {/* <!-- Add a SearchResults component --> */}
+      <SearchResults />
+      
       {/* <!-- Add a Playlist component --> */}
     </div>
   </div>
